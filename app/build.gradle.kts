588a1442b012b107a15d7769ef2e3ef80d67b59d
winterom <winterom@gmail.com>
2024-01-24 14:26:40 +0300

    feature: add settings screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "alexey.gritsenko.playlistmaker"
+        applicationId = "com.example.playlistmaker"
         minSdk = 29
         targetSdk = 33
         versionCode = 1
@@ -37,10 +37,9 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.material:material:1.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
