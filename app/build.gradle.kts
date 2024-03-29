a0e363e39b942f25ed6874b90cbb4213930f442b
winterom <winterom@gmail.com>
2024-03-29 13:35:07 +0300

    feature: player view

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,6 @@ dependencies {
     implementation("com.google.android.material:material:1.9.0")
 
     implementation("com.github.bumptech.glide:glide:4.16.0")
-    implementation("androidx.activity:activity:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
 
---
+++ app/src/main/java/alexey/gritsenko/playlistmaker/activity/playeractivity/PlayerActivity.kt
@@ -0,0 +1,12 @@
+package alexey.gritsenko.playlistmaker.activity.playeractivity
+
+import alexey.gritsenko.playlistmaker.R
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player)
+    }
+}
