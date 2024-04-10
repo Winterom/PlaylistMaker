2558ef45f7ee07756ea1fbe762d14c2f4fe2a6c5
winterom <winterom@gmail.com>
2024-04-10 17:55:18 +0300

    fix: player view

--- app/src/main/java/alexey/gritsenko/playlistmaker/activity/playeractivity/PlayerActivity.kt
+++ app/src/main/java/alexey/gritsenko/playlistmaker/activity/playeractivity/PlayerActivity.kt
@@ -15,6 +15,15 @@ class PlayerActivity : AppCompatActivity() {
     private lateinit var track: Track
     private lateinit var timerTextView:TextView
     private lateinit var messageTextView:TextView
+    private lateinit var image:ImageView
+    private lateinit var duration:TextView
+    private lateinit var albumName:TextView
+    private lateinit var albumNameTitle:TextView
+    private lateinit var trackName:TextView
+    private lateinit var artistName:TextView
+    private lateinit var trackYear:TextView
+    private lateinit var trackCountry:TextView
+    private lateinit var trackGenre:TextView
     companion object{
         const val TRACK ="TRACK"
     }
@@ -22,30 +31,32 @@ class PlayerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player)
         initTrack()
-        initReturnButton()
-        initPlaceholderImage()
-        initTrackName()
-        initArtistName()
-        initTimerTextView()
-        initDurationGroup()
-        initAlbumGroup()
-        initYearTextView()
-        initCountryTextView()
-        initMessageTextView()
+        initViews()
+        completeViews()
     }
 
     private fun initTrack() {
         track = intent.getSerializableExtra(TRACK) as Track
     }
-    private fun initReturnButton(){
-        val returnButton = findViewById<ImageView>(id.return_to_main)
-        returnButton.setOnClickListener {
-            finish()
-        }
-        println(track)
+    private fun initViews() {
+       findViewById<ImageView>(id.return_to_main)
+           .setOnClickListener {
+            finish() }
+        image = findViewById(id.placeholder_image_view)
+        timerTextView = findViewById(id.track_timer_text_view)
+        duration = findViewById(id.track_duration_text_view)
+        albumName = findViewById(id.track_album_text_view)
+        albumNameTitle = findViewById(id.track_album_title_text_view)
+        trackName = findViewById(id.track_name_text_view)
+        artistName = findViewById(id.track_item_artist_text_view)
+        trackYear = findViewById(id.track_year_text_view)
+        trackCountry = findViewById(id.track_country_text_view)
+        messageTextView = findViewById(id.playlist_is_created_text_view)
+        trackGenre = findViewById(id.track_genre_text_view)
+        messageTextView.isVisible=false
     }
-    private fun initPlaceholderImage() {
-        val image = findViewById<ImageView>(id.placeholder_image_view)
+
+    private fun completeViews(){
         if (track.artworkUrl100 == null) {
             image.setImageResource(R.drawable.placeholder)
         } else {
@@ -55,38 +66,20 @@ class PlayerActivity : AppCompatActivity() {
                 .placeholder(R.drawable.placeholder)
                 .into(image)
         }
-    }
-    private fun initTimerTextView(){
-        timerTextView = findViewById(id.track_timer_text_view)
-        timerTextView.text = "0:00"//mock
+        duration.text=track.trackTime
+
+        trackName.text=track.trackName
+        artistName.text=track.artistName
+        trackYear.text=track.releaseDate
+        trackCountry.text=track.country
+        timerTextView.text = track.trackTime
+        trackGenre.text = track.primaryGenreName
+        if(track.collectionName==null){
+            albumNameTitle.isVisible=false
+            albumName.isVisible=false
+        }else{
+            albumName.text=track.collectionName
+        }
     }
 
-    private fun initDurationGroup(){
-        val duration = findViewById<TextView>(id.track_duration_text_view)
-        duration.text=track.trackTime
-    }
-    private fun initAlbumGroup(){
-        val album = findViewById<TextView>(id.track_album_text_view)
-        album.text=track.collectionName
-    }
-    private fun initTrackName(){
-        val name = findViewById<TextView>(id.track_name_text_view)
-        name.text=track.trackName
-    }
-    private fun initArtistName(){
-        val artist = findViewById<TextView>(id.track_item_artist_text_view)
-        artist.text=track.artistName
-    }
-    private fun initYearTextView(){
-        val year = findViewById<TextView>(id.track_year_text_view)
-        year.text=track.releaseDate
-    }
-    private fun initCountryTextView(){
-        val country = findViewById<TextView>(id.track_country_text_view)
-        country.text=track.country
-    }
-    private fun initMessageTextView(){
-        messageTextView = findViewById(id.playlist_is_created_text_view)
-        messageTextView.isVisible=false
-    }
 }
