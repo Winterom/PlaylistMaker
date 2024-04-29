45af0f4652df6e8979f6b44601aea6c34024be3b
winterom <winterom@gmail.com>
2024-04-29 14:10:29 +0300

    fix: timerView

--- app/src/main/java/alexey/gritsenko/playlistmaker/activity/playeractivity/TimerView.kt
+++ app/src/main/java/alexey/gritsenko/playlistmaker/activity/playeractivity/TimerView.kt
@@ -1,33 +1,30 @@
 package alexey.gritsenko.playlistmaker.activity.playeractivity
 
+import android.media.MediaPlayer
 import android.os.Handler
 import android.os.Looper
 import android.widget.TextView
 
-class TimerView(private val textView: TextView) {
-    private var startTime: Int = 0
+class TimerView(private val textView: TextView,
+                private val mediaPlayer: MediaPlayer) {
+
     private val timerHandler = Handler(Looper.getMainLooper())
     private var timerRunnable: Runnable = object : Runnable {
         override fun run() {
-            var seconds = startTime
+            var seconds = mediaPlayer.currentPosition/ 1000
             val minutes = seconds / 60
             seconds %= 60
-            startTime++
             textView.text = String.format("%d:%02d", minutes, seconds)
             timerHandler.postDelayed(this, 1000)
         }
     }
 
-
     fun start(){
         timerHandler.postDelayed(timerRunnable, 1000)
     }
 
-    fun pause(){
+    fun stop(){
         timerHandler.removeCallbacks(timerRunnable)
     }
-    fun reset(){
-        timerHandler.removeCallbacks(timerRunnable)
-        startTime=0
-    }
+
 }
