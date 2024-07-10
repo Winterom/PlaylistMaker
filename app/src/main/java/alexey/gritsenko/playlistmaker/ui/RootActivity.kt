80ae53e90bfc6eeff64447d8484c9d525daff636
winterom <winterom@gmail.com>
2024-07-10 16:02:21 +0300

    feature: fragments navigation

--- app/src/main/java/alexey/gritsenko/playlistmaker/ui/RootActivity.kt
+++ app/src/main/java/alexey/gritsenko/playlistmaker/ui/RootActivity.kt
@@ -11,14 +11,18 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class RootActivity: AppCompatActivity() {
 
     private lateinit var binding:ActivityRootBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRootBinding.inflate(layoutInflater)
         setContentView(binding.root)
+       initialize()
+
+    }
+    private fun initialize(){
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment
         val navController = navHostFragment.navController
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomNavigationView.setupWithNavController(navController)
+        bottomNavigationView.itemTextColor = null
     }
 }
